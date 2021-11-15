36326c597b1f7ee10a522864e53c7b3b29734f3f
Brady Buchholz <bradybuchholz@gmail.com>
2021-11-15 14:38:52 -0500

    Ignore

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sequelize  = require('./config/connection');
+const db = require('./models');
+const hbs =  require('express-handlebars');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+    res.send("Homepage!");
+});
+
+sequelize.sync({force:true}).then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server is listening at http://localhost:${PORT}`)
+    });
+});
