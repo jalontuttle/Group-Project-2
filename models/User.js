363d43ef57e13732bf5bf543f968c5c235a6dd53
Brady Buchholz <bradybuchholz@gmail.com>
2021-11-16 13:17:45 -0500

    Added user seeds,and userRoutes. Also added a user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
 
 User.init(
     {
@@ -26,6 +30,16 @@ User.init(
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                len: [8],
+        },},
+    },
+    {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
         },
     },
     {
