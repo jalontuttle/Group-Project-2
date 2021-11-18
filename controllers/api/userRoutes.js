6b7ceb5cb79c24e36aa24a5183c669500d5829a5
Jalon Tuttle <89105006+jalontuttle@users.noreply.github.com>
2021-11-18 11:11:55 -0500

    Revert "Login logout"

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,50 +3,41 @@ const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
-      const userInfo = await User.findOne({ where: { email: req.body.email } });
-      console.log(req.body);
-      if(!userInfo) {
-          res.status(400).json({ message: 'Incorrect email or password, please try again'});
-          return;
-      }
-      console.log('pass userInfo');
-      const validPassword = await userInfo.checkPassword(req.body.password);
-      console.log(validPassword);
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-      // Create session variables based on the logged in user
-      req.session.save(() => {
-        req.session.user_id = userInfo.id;
-        req.session.logged_in = true;
-        res.json({ user: userInfo, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      res.status(400).json(err);
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
-router.post('/', async (req, res) => {
-try{
-  const userData = await User.create({
-    user_name: req.body.user_name,
-    email: req.body.email,
-    password: req.body.password
-  })
-  req.session.save(() => {
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
-    res.json({ user: userData, message: 'You are now logged in!' });
-  });
-} catch (err) {
-  res.status(400).json(err);
-}
-});
+
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
-   console.log("destroying session")
     // Remove the session variables
     req.session.destroy(() => {
       res.status(204).end();
@@ -55,4 +46,5 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
 module.exports = router;
